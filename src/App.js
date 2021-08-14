589f2e2e7d3fa2e65b753365e74517c299e51c7a
Ruban Sahoo <rubansahoo1@gmail.com>
2021-08-14 14:54:26 +0530

    updated api url

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     results: [],
     selected: {}
   });
-  const apiurl = "http://www.omdbapi.com/?apikey=11a5d7c2";
+  const apiurl = "https://www.omdbapi.com/?apikey=11a5d7c2";
 
   const search = (e) => {
     if (e.key === "Enter") {
